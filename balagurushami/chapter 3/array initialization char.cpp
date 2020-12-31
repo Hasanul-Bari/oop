302f400ed6a6e99bfeb66d347e8967c03b4f2d16
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-12-31 19:59:37 +0600

    remove

--- balagurushami/chapter 3/array initialization char.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{    char a[4]="abc";
-    cout<<a;
-}
-
--- balagurushami/chapter 3/debugging exercise/3.1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    int i=0;
-    i=400*400/400;
-    cout<<i;
-}
